323fb41c380440e536a754547b5a7d90b3c76cbc
Petr Sima <petrsima@icloud.com>
2023-11-02 11:41:05 +0100

    added View+TestTask sugar to tests

--- Standups-MVCTests/StandupListTests.swift
+++ Standups-MVCTests/StandupListTests.swift
@@ -10,67 +10,39 @@ import SwiftUI
 import SwiftData
 
 final class StandupListTests: XCTestCase {
-  
-  var window: UIWindow {
-    (UIApplication.shared.connectedScenes.first as! UIWindowScene).windows.first!
-  }
-  
+    
   @MainActor
   func testAdd() async throws {
     let modelContainer = try! ModelContainer(for: Standup.self, configurations: .init(isStoredInMemoryOnly: true))
     modelContainer.mainContext.insert(Standup.sample)
-    struct TestView: View {
-      var modelContainer: ModelContainer
-      @State private var testPreference: TestPreference = .init()
-      
-      var body: some View {
-        return StandupsListView()
-          .modelContainer(modelContainer)
-          .task {
-            // test
-            try! await Task.sleep()
-            try! await Task.sleep()
+    try await StandupsListView()
+      .modelContainer(modelContainer)
+      .testTask { $testPreference in
+        try! await Task.sleep()
+        try! await Task.sleep()
 
-            XCTAssertEqual(testPreference[\StandupsListView.standups].map(\.title), ["Daily Standup"])
-            
-            testPreference[\StandupsListView.addStandup]()
-            try! await Task.sleep(until: .now + .seconds(1), clock: .suspending)
-            testPreference[\EditStandupView.$standup.title].wrappedValue = "hello"
-            try! await Task.sleep(until: .now + .seconds(1), clock: .suspending)
-            testPreference[\EditStandupView.save]()
-            try! await Task.sleep(until: .now + .seconds(1), clock: .suspending)
-            let standups = testPreference[\StandupsListView.standups]
-            XCTAssertEqual(standups.map(\.title).sorted(), ["Daily Standup", "hello"].sorted())
-          }
-          .onPreferenceChange(TestPreference.self, perform: { value in
-            testPreference = value
-          })
+        XCTAssertEqual(testPreference[\StandupsListView.standups].map(\.title), ["Daily Standup"])
+        
+        testPreference[\StandupsListView.addStandup]()
+        try! await Task.sleep(until: .now + .seconds(1), clock: .suspending)
+        testPreference[\EditStandupView.$standup.title].wrappedValue = "hello"
+        try! await Task.sleep(until: .now + .seconds(1), clock: .suspending)
+        testPreference[\EditStandupView.save]()
+        try! await Task.sleep(until: .now + .seconds(1), clock: .suspending)
+        let standups = testPreference[\StandupsListView.standups]
+        XCTAssertEqual(standups.map(\.title).sorted(), ["Daily Standup", "hello"].sorted())
       }
-    }
-    
-    let v = TestView(modelContainer: modelContainer)
-    let host = UIHostingController(rootView: v)
-    window.rootViewController = host
-    window.makeKeyAndVisible()
-    
-    try await Task.sleep(until: .now + .seconds(5))
   }
   
   
   @MainActor
   func testEdit() async throws {
-    struct TestView: View {
-//      @State private var editButtonAction: () -> Void = { }
-//      @State private var titleBinding: Binding<String> = .constant("")
-      @State private var testPreference: TestPreference = .init()
-      
-      var body: some View {
-        let modelContainer = try! ModelContainer(for: Standup.self, configurations: .init(isStoredInMemoryOnly: true))
-        //This test sometimes fails if we have multiple standups 🤔
-        [Standup.sample].forEach(modelContainer.mainContext.insert)
-        return StandupsListView()
+    let modelContainer = try! ModelContainer(for: Standup.self, configurations: .init(isStoredInMemoryOnly: true))
+    //This test sometimes fails if we have multiple standups 🤔
+    [Standup.sample].forEach(modelContainer.mainContext.insert)
+    try await StandupsListView()
           .modelContainer(modelContainer)
-          .task {
+          .testTask { $testPreference in
             // test
             try! await Task.sleep()
             try! await Task.sleep()
@@ -84,18 +56,45 @@ final class StandupListTests: XCTestCase {
             try! await Task.sleep(until: .now + .seconds(1))
             XCTAssertEqual(testPreference[\StandupsListView.standups][0].title, "hey")
           }
-          .onPreferenceChange(TestPreference.self, perform: { value in
-            print("test preference changed", value)
-            testPreference = value
-          })
-      }
-    }
-    
-    let v = TestView()
-    let host = UIHostingController(rootView: v)
-    window.rootViewController = host
-    window.makeKeyAndVisible()
-    
-    try await Task.sleep(until: .now + .seconds(4))
+         
   }
 }
+
+extension View {
+  @MainActor
+  func testTask(_ task: @MainActor @escaping (Binding<TestPreference>) async throws -> Void) async throws {
+    let testedView = modifier(
+      TestTaskViewModifier(task: task)
+    )
+    let host = UIHostingController(rootView: testedView)
+    testWindow.rootViewController = host
+    testWindow.makeKeyAndVisible()
+    
+    try await Task.sleep(until: .now + .seconds(4)) //TODO: wait for test task to finish
+  }
+}
+
+
+struct TestTaskViewModifier: ViewModifier {
+  @State private var testPreference: TestPreference = .init()
+  var task: @MainActor (Binding<TestPreference>) async throws -> Void
+  func body(content: Content) -> some View {
+    content
+      .onPreferenceChange(TestPreference.self, perform: { value in
+        testPreference = value
+      })
+      .task {
+        do {
+          try await task($testPreference)
+        } catch {
+          XCTFail(error.localizedDescription)
+        }
+      }
+  }
+}
+
+// this needs to be the real app key window on some platforms, custom one has problems
+var testWindow: UIWindow {
+  (UIApplication.shared.connectedScenes.first as! UIWindowScene).windows.first!
+}
+
